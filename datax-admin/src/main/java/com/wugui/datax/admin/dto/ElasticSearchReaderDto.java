1e1191ad8867ffea6985be506e21756be68bdabc
xuchenglei <599903582@qq.com>
2020-09-22 16:25:59 +0800

    ElasticSearch Dto的书写

---
+++ datax-admin/src/main/java/com/wugui/datax/admin/dto/ElasticSearchReaderDto.java
@@ -0,0 +1,16 @@
+package com.wugui.datax.admin.dto;
+
+import java.io.Serializable;
+
+/**
+ * @author Mr.Xu
+ * @create 2020-09-22 16:00
+ */
+public class ElasticSearchReaderDto implements Serializable {
+
+    // 文档索引名称
+    private String indexName;
+
+    // 文档ID
+    private String document;
+}
